e7cd521016b5ebf4b26a9b85cab81e09b43c3928
FacuA0 <facundoamarilla28@gmail.com>
2025-05-11 04:11:38 -0300

    Added some transition animations to eyelids

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let activado = false;
 let control, gBuho, buho, cara, mascara, ojoIzq, ojoDer, basePico;
+let ojoIzqPup, ojoIzqRef, ojoIzqBol, ojoIzqPar, ojoDerPup, ojoDerRef, ojoDerBol, ojoDerPar;
 
 window.onload = function() {
     control = document.querySelector("svg");
@@ -8,7 +9,15 @@ window.onload = function() {
     cara = document.querySelector("#cara");
     mascara = document.querySelector("#mascara");
     ojoIzq = document.querySelector("#ojo-izq");
+    ojoIzqPup = document.querySelector("#ojo-izq-pup");
+    ojoIzqRef = document.querySelector("#ojo-izq-ref");
+    ojoIzqBol = document.querySelector("#ojo-izq-bolsa");
+    ojoIzqPar = document.querySelector("#ojo-izq-parpado");
     ojoDer = document.querySelector("#ojo-der");
+    ojoDerPup = document.querySelector("#ojo-der-pup");
+    ojoDerRef = document.querySelector("#ojo-der-ref");
+    ojoDerBol = document.querySelector("#ojo-der-bolsa");
+    ojoDerPar = document.querySelector("#ojo-der-parpado");
     basePico = document.querySelector("#base-pico");
 
     control.addEventListener("click", () => {
@@ -20,8 +29,16 @@ window.onload = function() {
         agregarAnimaciones(buho, "buho");
         agregarAnimaciones(cara, "cara");
         agregarAnimaciones(mascara, "mascara");
-        agregarAnimaciones(ojoIzq, "ojo-izq");
-        agregarAnimaciones(ojoDer, "ojo-der");
+        //agregarAnimaciones(ojoIzq, "ojo-izq");
+        agregarAnimaciones(ojoIzqPup, "ojo-izq-pup");
+        agregarAnimaciones(ojoIzqRef, "ojo-izq-ref");
+        agregarAnimaciones(ojoIzqBol, "ojo-izq-bolsa");
+        agregarAnimaciones(ojoIzqPar, "ojo-izq-parpado");
+        //agregarAnimaciones(ojoDer, "ojo-der");
+        agregarAnimaciones(ojoDerPup, "ojo-der-pup");
+        agregarAnimaciones(ojoDerRef, "ojo-der-ref");
+        agregarAnimaciones(ojoDerBol, "ojo-der-bolsa");
+        agregarAnimaciones(ojoDerPar, "ojo-der-parpado");
         agregarAnimaciones(basePico, "base-pico");
     });
 };

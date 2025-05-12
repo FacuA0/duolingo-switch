14cf1b16cdb42dbfe68a9f651ba4c9c1709bf125
FacuA0 <facundoamarilla28@gmail.com>
2025-05-12 02:05:49 -0300

    Moved animations to a new file and did all other elements' animations

--- script.js
+++ script.js
@@ -1,49 +1,40 @@
 let activado = false;
-let control, gBuho, buho, cara, mascara, ojoIzq, ojoDer, basePico;
+let control, fondo, gBuho, buho, cara, mascara, basePico;
 let ojoIzqPup, ojoIzqRef, ojoIzqBol, ojoIzqPar, ojoDerPup, ojoDerRef, ojoDerBol, ojoDerPar;
 
 window.onload = function() {
     control = document.querySelector("svg");
-    gBuho = document.querySelector("#g-buho");
-    buho = document.querySelector("#buho");
-    cara = document.querySelector("#cara");
-    mascara = document.querySelector("#mascara");
-    ojoIzq = document.querySelector("#ojo-izq");
-    ojoIzqPup = document.querySelector("#ojo-izq-pup");
-    ojoIzqRef = document.querySelector("#ojo-izq-ref");
-    ojoIzqBol = document.querySelector("#ojo-izq-bolsa");
-    ojoIzqPar = document.querySelector("#ojo-izq-parpado");
-    ojoDer = document.querySelector("#ojo-der");
-    ojoDerPup = document.querySelector("#ojo-der-pup");
-    ojoDerRef = document.querySelector("#ojo-der-ref");
-    ojoDerBol = document.querySelector("#ojo-der-bolsa");
-    ojoDerPar = document.querySelector("#ojo-der-parpado");
-    basePico = document.querySelector("#base-pico");
-
     control.addEventListener("click", () => {
         activado = !activado;
 
         control.classList.toggle("dia");
 
-        agregarAnimaciones(gBuho, "g-buho");
-        agregarAnimaciones(buho, "buho");
-        agregarAnimaciones(cara, "cara");
-        agregarAnimaciones(mascara, "mascara");
-        //agregarAnimaciones(ojoIzq, "ojo-izq");
-        agregarAnimaciones(ojoIzqPup, "ojo-izq-pup");
-        agregarAnimaciones(ojoIzqRef, "ojo-izq-ref");
-        agregarAnimaciones(ojoIzqBol, "ojo-izq-bolsa");
-        agregarAnimaciones(ojoIzqPar, "ojo-izq-parpado");
-        //agregarAnimaciones(ojoDer, "ojo-der");
-        agregarAnimaciones(ojoDerPup, "ojo-der-pup");
-        agregarAnimaciones(ojoDerRef, "ojo-der-ref");
-        agregarAnimaciones(ojoDerBol, "ojo-der-bolsa");
-        agregarAnimaciones(ojoDerPar, "ojo-der-parpado");
-        agregarAnimaciones(basePico, "base-pico");
+        agregarAnimaciones("fondo");
+        agregarAnimaciones("g-astro");
+        agregarAnimaciones("astro");
+        agregarAnimaciones("astro-sup");
+        agregarAnimaciones("astro-inf");
+        agregarAnimaciones("astro-crateres");
+        agregarAnimaciones("astro-sombra");
+        agregarAnimaciones("nubes");
+        agregarAnimaciones("g-buho");
+        agregarAnimaciones("buho");
+        agregarAnimaciones("cara");
+        agregarAnimaciones("mascara");
+        agregarAnimaciones("ojo-izq-pup");
+        agregarAnimaciones("ojo-izq-ref");
+        agregarAnimaciones("ojo-izq-bolsa");
+        agregarAnimaciones("ojo-izq-parpado");
+        agregarAnimaciones("ojo-der-pup");
+        agregarAnimaciones("ojo-der-ref");
+        agregarAnimaciones("ojo-der-bolsa");
+        agregarAnimaciones("ojo-der-parpado");
+        agregarAnimaciones("base-pico");
     });
 };
 
-function agregarAnimaciones(elem, nombre) {
+function agregarAnimaciones(nombre) {
+    let elem = document.querySelector("#" + nombre);
     elem.style.animationName = !activado ? "noche-" + nombre : "dia-" + nombre;
     elem.addEventListener("animationend", () => {
         //elem.style.animationName = "none";
